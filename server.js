c5ccba9dfd21e6376ec570ceaa524147fbd77c5d
Walter <zikavaclav05@gmail.com>
2023-03-06 21:51:31 +0100

    new

--- server.js
+++ server.js
@@ -4,6 +4,32 @@ const cors = require("cors");
 
 app.use(cors())
 
+app.get("/get_api_links", async (req, res) => {
+    // First layer of stealing the links (https://seapi.link)
+    const id = req.query.id;
+    const season = req.query.s;
+    const episode = req.query.e;
+
+    if (!id)
+        // Return if ID missing
+        res.json({
+                "status" : "error",
+                "reason" : "id_missing",
+        });
+
+    // Get response based on series or movies
+    let url = "https://seapi.link/?type=tmdb&id="+id;+"&max_results=1";
+    if (season != undefined && episode != undefined)
+        url = "https://seapi.link/?type=tmdb&id="+id+"&season="+season+"&episode="+episode+"&max_results=1";
+
+    // Fetch and get json
+    const apiLinks = await fetch(url);
+    const jsonLinks = await apiLinks.json();
+    res.json({
+        "status" : "success",
+        "response" : jsonLinks
+    });
+})
 
 app.get("/", async (req, res) => {
     console.log(req.query.url);
@@ -19,7 +45,6 @@ app.get("/", async (req, res) => {
         res.json({"pirate_link" : "", "status" : "captcha"});
     }
     const realLink = anotherRes.split("window.atob('")[1].split("')+")[0];
-    console.log(s);
     res.json({"pirate_link" : realLink, "status" : "success"});
 })
 
